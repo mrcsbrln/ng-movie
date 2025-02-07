d7575a1e916892732d827c7adf8c9260d9fd8c97
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-02-07 16:49:05 +0100

    progress

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-slider',
   imports: [],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrl: './slider.component.scss',
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
+  private http: HttpClient = inject(HttpClient);
 
+  ngOnInit() {
+    console.log('Hello Component');
+  }
 }
