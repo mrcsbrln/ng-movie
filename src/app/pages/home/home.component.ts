2188516ed8fe65770c0729457fa6cd2558825354
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-02-07 15:34:05 +0100

    progress

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { SliderComponent } from '../../components/slider/slider.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SliderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
