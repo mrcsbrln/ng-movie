ff71b8fa927010802aea598a1e67206a736caf99
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-02-19 21:33:20 +0100

    progress

--- src/app/components/show-item/show-item.component.ts
+++ src/app/components/show-item/show-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Movie } from '../../interfaces/movie.interface';
 import { imagesBaseUrl } from '../../constants/images-sizes';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-show-item',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './show-item.component.html',
   styleUrl: './show-item.component.scss',
 })
