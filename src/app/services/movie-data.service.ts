3cb0d4765b9f8631bba73f11afec1ad6ff3600dc
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-02-19 22:47:08 +0100

    progress

--- src/app/services/movie-data.service.ts
+++ src/app/services/movie-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { GenresDto, Movie, MoviesDto } from '../interfaces/movie.interface';
 import { map } from 'rxjs';
@@ -13,70 +13,82 @@ import { environment } from '../../environments/environment';
 export class MovieDataService {
   private http = inject(HttpClient);
   private apiUrl = environment.apiUrl;
-  private apiKey = environment.apiKey;
+  private bearerToken = environment.bearerToken;
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: `Bearer ${this.bearerToken}`,
+    });
+  }
 
   getMovieByType(type: string, count = 12) {
     return this.http
-      .get<MoviesDto>(`${this.apiUrl}/movie/${type}?api_key=${this.apiKey}`)
+      .get<MoviesDto>(`${this.apiUrl}/movie/${type}`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.results.slice(0, count)));
   }
 
   getMovieById(id: string) {
-    return this.http.get<Movie>(
-      `${this.apiUrl}/movie/${id}?api_key=${this.apiKey}`
-    );
+    return this.http.get<Movie>(`${this.apiUrl}/movie/${id}`, {
+      headers: this.getHeaders(),
+    });
   }
 
   getMovieVideos(id: string) {
     return this.http
-      .get<VideoDto>(`${this.apiUrl}/movie/${id}/videos?api_key=${this.apiKey}`)
+      .get<VideoDto>(`${this.apiUrl}/movie/${id}/videos`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.results));
   }
 
   getMovieImages(id: string) {
     return this.http
-      .get<ImageDto>(`${this.apiUrl}/movie/${id}/images?api_key=${this.apiKey}`)
+      .get<ImageDto>(`${this.apiUrl}/movie/${id}/images`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.backdrops));
   }
 
   getMovieCast(id: string) {
     return this.http
-      .get<CreditsDto>(
-        `${this.apiUrl}/movie/${id}/credits?api_key=${this.apiKey}`
-      )
+      .get<CreditsDto>(`${this.apiUrl}/movie/${id}/credits`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.cast));
   }
 
   getSimilarMovies(id: string, count = 12) {
     return this.http
-      .get<MoviesDto>(
-        `${this.apiUrl}/movie/${id}/similar?api_key=${this.apiKey}`
-      )
+      .get<MoviesDto>(`${this.apiUrl}/movie/${id}/similar`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.results.slice(0, count)));
   }
 
   searchMovies(page: number, searchValue?: string) {
     const uri = searchValue ? 'search/movie' : 'movie/popular';
     return this.http.get<MoviesDto>(
-      `${this.apiUrl}/${uri}?query=${searchValue}&page=${page}&api_key=${this.apiKey}`
+      `${this.apiUrl}/${uri}?query=${searchValue}&page=${page}`,
+      { headers: this.getHeaders() }
     );
   }
 
   getMoviesGenres() {
     return this.http
-      .get<GenresDto>(`${this.apiUrl}/genre/movie/list?api_key=${this.apiKey}`)
+      .get<GenresDto>(`${this.apiUrl}/genre/movie/list`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.genres));
   }
 
   getMoviesByGenre(genreId: string, pageNumber = 1) {
     return this.http
       .get<MoviesDto>(
-        `${this.apiUrl}/discover/movie?with_genres=${genreId}&page=${pageNumber}&api_key=${this.apiKey}`
+        `${this.apiUrl}/discover/movie?with_genres=${genreId}&page=${pageNumber}`,
+        { headers: this.getHeaders() }
       )
-      .pipe(
-        map((data) => {
-          return data.results;
-        })
-      );
+      .pipe(map((data) => data.results));
   }
 }
--- src/app/services/tvshows-data.service.ts
+++ src/app/services/tvshows-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map } from 'rxjs';
 import { Tvshow, TvshowsDto } from '../interfaces/tvshow.interface';
@@ -14,48 +14,65 @@ import { environment } from '../../environments/environment';
 export class TvshowsDataService {
   private http = inject(HttpClient);
   private apiUrl = environment.apiUrl;
-  private apiKey = environment.apiKey;
+  private bearerToken = environment.bearerToken; // Statt apiKey
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: `Bearer ${this.bearerToken}`,
+    });
+  }
 
   getTvshowsByType(type: string, count = 12) {
     return this.http
-      .get<TvshowsDto>(`${this.apiUrl}/tv/${type}?api_key=${this.apiKey}`)
+      .get<TvshowsDto>(`${this.apiUrl}/tv/${type}`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.results.slice(0, count)));
   }
 
   getTvshowById(id: string) {
-    return this.http.get<Tvshow>(
-      `${this.apiUrl}/tv/${id}?api_key=${this.apiKey}`
-    );
+    return this.http.get<Tvshow>(`${this.apiUrl}/tv/${id}`, {
+      headers: this.getHeaders(),
+    });
   }
 
   getTvshowVideos(id: string) {
     return this.http
-      .get<VideoDto>(`${this.apiUrl}/tv/${id}/videos?api_key=${this.apiKey}`)
+      .get<VideoDto>(`${this.apiUrl}/tv/${id}/videos`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.results));
   }
 
   getTvshowImages(id: string) {
     return this.http
-      .get<ImageDto>(`${this.apiUrl}/tv/${id}/images?api_key=${this.apiKey}`)
+      .get<ImageDto>(`${this.apiUrl}/tv/${id}/images`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.backdrops));
   }
 
   getTvshowCast(id: string) {
     return this.http
-      .get<CreditsDto>(`${this.apiUrl}/tv/${id}/credits?api_key=${this.apiKey}`)
+      .get<CreditsDto>(`${this.apiUrl}/tv/${id}/credits`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.cast));
   }
 
   getSimilarTvshows(id: string, count = 12) {
     return this.http
-      .get<MoviesDto>(`${this.apiUrl}/tv/${id}/similar?api_key=${this.apiKey}`)
+      .get<MoviesDto>(`${this.apiUrl}/tv/${id}/similar`, {
+        headers: this.getHeaders(),
+      })
       .pipe(map((data) => data.results.slice(0, count)));
   }
 
   searchTvshows(page: number, searchValue?: string) {
     const uri = searchValue ? 'search/tv' : 'tv/popular';
     return this.http.get<TvshowsDto>(
-      `${this.apiUrl}/${uri}?query=${searchValue}&page=${page}&api_key=${this.apiKey}`
+      `${this.apiUrl}/${uri}?query=${searchValue}&page=${page}`,
+      { headers: this.getHeaders() }
     );
   }
 }
